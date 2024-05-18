c0417fd82e7ea065600bb587cd4fc1be3fcadf94
Joko Comia <jokocomia05@gmail.com>
2024-05-18 17:28:41 +0800

    Added MemberTaskCard

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -273,6 +273,16 @@ namespace WorkHive.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Card___Tasks2 {
+            get {
+                object obj = ResourceManager.GetObject("Card - Tasks2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Confirmbtn {
             get {
                 object obj = ResourceManager.GetObject("Confirmbtn", resourceCulture);
